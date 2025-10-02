707fa25c8e85b05e0e999cd9247bbd4a8f644cb8
Aqu1bK <aquibkhan7221@gmail.com>
2025-10-02 22:15:14 +0530

    LinkedList part 2

---
+++ 24.Linked List/Recursive_Search.java
@@ -0,0 +1,160 @@
+public class Recursive_Search {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        //step1 = create new Node
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //step2 - newNode next = head
+        newNode.next = head; //link
+
+        //step3 - head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data) { 
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() { //O(n)
+        if(head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void add(int idx, int data) {
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+
+        //i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size-2
+        Node prev = head;
+        for(int i=0; i<size-2; i++) {
+            prev = prev.next;
+        }
+
+        int val = prev.next.data; //tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while(temp != null) {
+            if(temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        //key not found
+        return -1;
+    }
+
+    public int helper(Node head, int key) {
+        if(head == null){
+            return -1;
+        }
+
+        if(head.data == key) {
+            return 0;
+        }
+
+        int idx = helper(head.next, key);
+        if(idx == -1) {
+            return -1;
+        }
+        return idx+1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public static void main (String args[]) {
+        Recursive_Search ll = new Recursive_Search();
+        ll.addFirst(2); 
+        ll.addFirst(1);             
+        ll.addLast(4);     
+        ll.addLast(5);
+        ll.add(2, 3);
+
+        ll.print();
+        System.out.println(ll.recSearch(3));
+        System.out.println(ll.recSearch(10));
+    }
+}
---
+++ 25.Linked List (Part2)/LL_in_Collection_Framework.java
@@ -0,0 +1,20 @@
+// Linked List in Collection Framework
+import java.util.LinkedList;
+
+public class LL_in_Collection_Framework {
+    public static void main(String args[]) {
+        // create - 
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        // add 
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        //0->1->2
+        System.out.println(ll);
+        //remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+    }
+}
