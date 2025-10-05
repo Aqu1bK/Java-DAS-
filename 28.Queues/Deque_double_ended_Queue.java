93a0185ccd1a5dabda5e57d03a5145a1f83b1b12
Aqu1bK <aquibkhan7221@gmail.com>
2025-10-05 19:13:31 +0530

    queue

---
+++ 28.Queues/Deque_double_ended_Queue.java
@@ -0,0 +1,19 @@
+// Deque (Double Ended Queue)
+import java.util.*;
+
+public class Deque_double_ended_Queue {
+    public static void main(String args[]) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1); //1
+        deque.addFirst(2); //2 1
+        System.out.println(deque);
+        deque.removeFirst();
+        System.out.println(deque);
+
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
+    }
+}
---
+++ 28.Queues/Implement_Queue_using_Deque.java
@@ -0,0 +1,34 @@
+// Question 7
+// Stack and Queue using Deque
+import java.util.*;
+
+public class Implement_Queue_using_Deque {
+
+    static class Queue {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void add(int data) {
+            deque.addFirst(data);
+        }
+
+        public int remove() {
+            return deque.removeLast();
+        }
+
+        public int peek() {
+            return deque.getLast();
+        }
+    }
+    public static void main(String args[]) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("peek = " + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    }
+    
+}
