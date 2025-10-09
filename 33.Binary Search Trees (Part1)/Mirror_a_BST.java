f22d6ed469fca1147a224829ae5564ca1c49a4e1
Aqu1bK <aquibkhan7221@gmail.com>
2025-10-09 17:58:01 +0530

    BST Part 1

---
+++ 33.Binary Search Trees (Part1)/Mirror_a_BST.java
@@ -0,0 +1,62 @@
+// Mirror a BST
+public class Mirror_a_BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static Node createMirror(Node root) {
+        if(root == null) {
+            return null;
+        }
+
+        Node leftMirror = createMirror(root.left);
+        Node rightMirror = createMirror(root.right);
+
+        root.left = rightMirror;
+        root.right = leftMirror;
+
+        return root;
+    }
+
+    public static void preorder(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    
+    // Added inorder method
+    public static void inorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+    // Fixed main method return type
+    public static void main(String args[]) {
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+
+        inorder(root);
+        System.out.println();
+        root = createMirror(root);  // Changed from mirror() to createMirror()
+        preorder(root);
+    }
+}
