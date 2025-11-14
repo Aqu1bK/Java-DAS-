83c13262e0aeed79fe85a21f048c23e1a74016ef
Aqu1bK <aquibkhan7221@gmail.com>
2025-11-14 11:18:36 +0530

    Bellman_Ford

---
+++ LeetCode/169. Majority Element/Majority_Element_Using_Boyer_Moore.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Majority_Element_Using_Boyer_Moore {
+    public static int majorityElement(int[] nums) {
+        int freq = 0;
+        int ans = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            if(freq == 0) {
+                ans = nums[i];
+            }
+            if(ans == nums[i]) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {3, 2, 3, 2, 2, 2};
+        System.out.print(majorityElement(nums));
+    }
+}
--- LeetCode/169. Majority Element/Majority_Element_Using_HashMap.java
+++ LeetCode/169. Majority Element/Majority_Element_Using_HashMap.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class Majority_Element_Using_HashMap {
-    
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > n / 2) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+	int nums[] = {3,2,3,2,2,2};
+	System.out.print(majorityElement(nums));
+	}
 }
